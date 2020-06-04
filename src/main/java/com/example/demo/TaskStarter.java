7091f8e7b98edca319cebf9dae22faf5598d9e0a
ligl <ligl@1.com>
2020-06-04 16:07:35 +0800

    1

--- src/main/java/com/example/demo/TaskStarter.java
+++ src/main/java/com/example/demo/TaskStarter.java
@@ -17,10 +17,10 @@ public class TaskStarter implements ApplicationRunner {
     private static Map<String, String> cookieMap = new HashMap<>();
     private static Map<String, StealInfo> stealCounter = new ConcurrentHashMap<>();
     static {
-        cookieMap.put("181", "pt_key=AAJei9C2ADDVc7tIQxVN5U_eqAL99JIcaOx_FZ3ugG6Q1Xn4wYi2SWLzBN1pf4_t79RPAlrz3iE");
-        cookieMap.put("o152", "pt_key=AAJeWMxXADAhM5suAOxUdds4vH4UH3_3KdhxHJHjIyxewTiEfEAw7IqY3ddiaPC-JnHzGHoC6So");
-        cookieMap.put("171", "pt_key=AAJei9BUADBLQkfhbi75nZHOYz1uXd1f8UB8v5OYCTtS-7586YyeS1sllT9Nb-EYDmPG5NBye74");
-        cookieMap.put("xlyz", "pt_key=AAJeWNCsADDIfJF76rnm8t1hWnArZi1sPNgf_spKO-m8Ya3abGJHuH9zBXZorOi1HRJXeyn5RDo");
+        cookieMap.put("181", "pt_key=AAJer4inADBQ_dspgJqn95Xvcu17B3nsNCIAY4fLiGGHOjFf4qr05A_CU5kd2F1h2i9orEtY6lw");
+        cookieMap.put("o152", "pt_key=AAJe2J5PADCm0gKrF_7xLmT8DYxCkXQw9HwYqVxYhX2HDPnubO9n_cJgUyhYLgkoqrrpLCY0pLA");
+        cookieMap.put("171", "pt_key=AAJe2KtrADAt3K_jVe8TD7nciPrFyw5Yq4ExgU9uzS8ielkbmFmc-FFf9lHDDDAO6wXEDfahrh0");
+//        cookieMap.put("xlyz", "pt_key=AAJeWNCsADDIfJF76rnm8t1hWnArZi1sPNgf_spKO-m8Ya3abGJHuH9zBXZorOi1HRJXeyn5RDo");
         cookieMap.put("xjl", "pt_key=AAJefHCCADBM5zQ6CBThaPgrkhMqirvcMo92b5H2PBHrCxMsf_OM-2kQWVhhxJmFrp4oiyNLxz8");
         cookieMap.put("xqq", "pt_key=AAJefHlTADAjWH6GFrXKgpQx46v9Fu42khjkZqOM8T9KUjrtaKS_4ZyN2DBSBlKtrW0SCqB9YAw");
     }
